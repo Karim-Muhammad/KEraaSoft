faf4a13e2250ece2dc266eea42436d8d6de8f044
Karim-Muhammad <kimoomar007@gmail.com>
2023-09-22 18:10:07 +0300

    Not Found Page

--- Session5/FormPHP/views/errors/generic.php
+++ Session5/FormPHP/views/errors/generic.php
@@ -1,3 +1,16 @@
-<?php require_once base_path("views/partials/header.view.php") ?>
-<h1><?= $code ?></h1>
-<?php require_once base_path("views/partials/footer.view.php") ?>
+<?php require_once base_path("views/partials/header.php") ?>
+
+<div class="container mt-4">
+  <h1 class="text-danger fs-1">
+    <?= $code ?>
+
+    <span class="fs-4">
+      <?= $code == 404 ? "Page Not Found" : "Internal Server Error" ?>
+    </span>
+  </h1>
+  <p>
+    <a class="btn btn-primary" href="/">Go Back</a>
+  </p>
+</div>
+
+<?php require_once base_path("views/partials/footer.php") ?>
