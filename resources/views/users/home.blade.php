34cdc21cb98ecdf64e494b4fa249d3c9de479427
Karim-Muhammad <kimoomar007@gmail.com>
2023-11-24 23:51:46 +0200

    directives

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -2,4 +2,25 @@
 
 @section('content')
 <h1>Student Home Page</h1>
+
+@if(true)
+    <p>This only displays if true</p>
+@endif
+
+@unless(true)
+    <p>This only displays if false</p>
+@endunless
+
+@for($i = 0; $i < 5; $i++)
+    <p>{{ $i + 1 }}. Iteration</p>
+@endfor
+
+@foreach([['id'=> 1], ['id': 2]] as $user)
+    <p>This is user {{ $user->id }}</p>
+    <div>
+        {{$loop->iteration}}
+    </div>
+@endforeach
+
+
 @endsection
